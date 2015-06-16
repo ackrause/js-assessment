3d08e88f927367c3d1cb74c0941c5b05faade43c
Andrew Krause <a.c.krause@gmail.com>
2015-06-16 08:03:15 -0700

    [UPDATE] Count answers

--- app/count.js
+++ app/count.js
@@ -2,6 +2,21 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.countAnswers =  {
   count : function (start, end) {
+    var id;
 
+    function counter() {
+      console.log(start++);
+      if (start <= end) {
+        id = window.setTimeout(counter, 100);
+      }
+    }
+
+    counter();
+
+    return {
+      cancel: function cancel() {
+        id && window.clearInterval(id);
+      }
+    }
   }
 };
