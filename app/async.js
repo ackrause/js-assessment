17e419e562e748ab6e4ea2a1e96e85f6ef32bf6a
Andrew Krause <a.c.krause@gmail.com>
2015-06-16 07:30:22 -0700

    [UPDATE] Async answers

--- app/async.js
+++ app/async.js
@@ -2,10 +2,31 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.asyncAnswers = {
   async : function(value) {
-
+    return new Promise(function (resolve, reject) {
+      return resolve(value);
+    });
   },
 
   manipulateRemoteData : function(url) {
+    return new Promise(function (resolve, reject) {
+      var client = new window.XMLHttpRequest();
+      client.open('GET', url);
+      client.send();
 
+      client.onload = function ajaxComplete() {
+        if (this.status === 200) {
+          var remoteData = JSON.parse(this.response),
+            names = remoteData.people.map(function getNames(person) {
+              return person.name;
+            }).sort();
+          resolve(names);
+        } else {
+          reject(this.statusText);
+        }
+      }
+      client.onerror = function ajaxFail() {
+        reject(this.statusText);
+      }
+    });
   }
 };
