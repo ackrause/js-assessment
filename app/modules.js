2d68d0edc99fb373083ad44a2cf8f978bd7796b4
Andrew Krause <a.c.krause@gmail.com>
2015-06-16 20:48:55 -0700

    [UPDATE] Modules answer

--- app/modules.js
+++ app/modules.js
@@ -2,6 +2,13 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.modulesAnswers = {
   createModule : function(str1, str2) {
+    return {
+      sayIt: function sayIt() {
+        return this.greeting + ', ' + this.name;
+      },
 
+      greeting: str1,
+      name: str2
+    };
   }
 };
