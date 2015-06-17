a9d6cf3771a5a02d8c185de8baa028100d08d708
Andrew Krause <a.c.krause@gmail.com>
2015-06-16 21:11:19 -0700

    [UPDATE] Objects answers

--- app/objects.js
+++ app/objects.js
@@ -2,14 +2,23 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.objectsAnswers =  {
   alterContext : function(fn, obj) {
-
+    var args = Array.prototype.slice.call(arguments, 2);
+    return fn.apply(obj, args);
   },
 
   alterObjects : function(constructor, greeting) {
-
+    constructor.prototype.greeting = greeting;
   },
 
   iterate : function(obj) {
+    var result = [];
 
+    for (var key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        result.push(key + ': ' + obj[key]);
+      }
+    }
+
+    return result;
   }
 };
